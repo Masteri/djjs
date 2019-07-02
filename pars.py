434cf0d684a90292e513dc64b8f3c3ea83ebd32f
master <manivan.iv@gmail.com>
2019-07-01 22:33:52 -0400

    get 2 lists

--- pars.py
+++ pars.py
@@ -6,40 +6,28 @@ url = ("http://www.alsautopa.com/vehiclelist.php?SortOrder=1&SortDirection=DESC&
 r = requests.get(url)
 soup = BeautifulSoup(r.text)
 list1 = soup.find("table", id="rsTable")
-
-
-#for item in soup.select("td.style14"):
-#    print(item.get_text())
-
-list3 = []
+tdtext = []
+tdhref = []
 
 for tr in list1.select("tr"):
-    print('tr---') #print(list1.index(tr))
-    for td in tr.find_all("td"):#class_="style14"):
+    for td in tr.find_all("td"):
+        i = ({list1.index(tr): td.text})
+        tdtext.append(i)
         if td.find('a'):
             if td.img:
-                print(td.img['src'])
-            print(td.text, td.a.get('href'))#, td.img.get('srd'))
-        print(td.text)
-        #print(list1.index(tr))
-        print('td--')
+                hr = ({list1.index(tr): 'http://www.alsautopa.com/' + td.img['src']})
+                tdhref.append(hr)
 
 
-
-
-#for td in list1.parent.find_all_next('td'):
-#    list3.append(td.text)
-
-#for el in list2[6:]:
-    #list3.append(el.a.get('href'))
-
+print('tdtext: ', tdtext)
+print('tdhref: ', tdhref)
 
 #listword = ['href', 'img', 'stock', 'year', 'model', 'make', 'title', 'description', 'price', '0', '1', '2', '3']
 #dictory = dict(zip(listword, list3))
 #print(dictory)
 
-#with io.open('data.txt', 'w', encoding='utf-8') as f:
-#  f.write(json.dumps(list3, ensure_ascii=False))
+with io.open('data.txt', 'w', encoding='utf-8') as f:
+  f.write(json.dumps(tdtext, ensure_ascii=False))
 
 
 #import json
